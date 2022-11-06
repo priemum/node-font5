cb3619b7da6622d227751ce8a0535b4d1b3f52be
gshemdoe <georgehmariki@gmail.com>
2022-11-07 01:35:09 +0300

    rate limitter added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const getRouter = require('./routes/get-routes')
+const elimit = require('express-rate-limit')
 
 const app = express()
 
@@ -12,12 +13,21 @@ mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@nodetuts
     console.log(err)
 })
 
+const limiter = elimit({
+	windowMs: 60 * 1000, // 1 minute
+	max: 7, // Limit each IP to 7 requests per `window` (here, per 1 minute)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: "To many request, please try again after 3 minutes"
+})
+
 // MIDDLEWARES
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 app.set('trust proxy', true) //our app is hosted on server using proxy to pass user request
+app.use(limiter)
 app.use(getRouter)
 
 
