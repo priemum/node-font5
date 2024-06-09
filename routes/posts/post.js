182ef2332fe579c0ffe095828d9a7c44856be830
gshemdoe <georgehmariki@gmail.com>
2024-06-09 04:00:12 +0300

    small updated

--- routes/posts/post.js
+++ routes/posts/post.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { bot1Fn } = require('./bots/bot1/bot1')
 
-router.post('webhook/bot1', async (req, res)=> {
+router.post('/webhook/bot1', async (req, res)=> {
     try {
         await bot1Fn()
     } catch (error) {
