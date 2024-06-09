7c836eca66fe5179be2506541c1b07c706de69a4
gshemdoe <georgehmariki@gmail.com>
2024-06-09 12:25:16 +0300

    bot removed

--- bots/pricebots/infos/info.js
+++ bots/pricebots/infos/info.js
@@ -1,22 +1,5 @@
 const info = [
-    {
-        TOKEN: '6914777890:AAHXkH8Ocbu5Emz0dR1Pj3DsfbCikLXacds',
-        SYMBOL: `arr`,
-        NAME: `Pirate Chain Token (ARRR)`,
-        PATH: `pirate-chain`
-    },
-    {
-        TOKEN: '7108389133:AAHSYFRlA-_fDpmVMefnQzKnv5F-6VCzfzk',
-        SYMBOL: `pbr`,
-        NAME: `PolkaBridge (PBR)`,
-        PATH: `polkabridge`
-    },
-    {
-        TOKEN: '7152960037:AAF9aHP6wEVyybhTDRixd_oF44dYht-uVhw',
-        SYMBOL: `dydx`,
-        NAME: `dYdX (DYDX)`,
-        PATH: `dydx`
-    },
+
 ]
 
 module.exports = info
