b34bbe1fdd35da17e3c4f001c08a9cdf85e7e183
gshemdoe <georgehmariki@gmail.com>
2024-06-09 04:23:51 +0300

    bot2 added

---
+++ routes/posts/bots/bot2/bot2.js
@@ -0,0 +1,36 @@
+const { Telegraf } = require('telegraf')
+
+const bot2Fn = async () => {
+    try {
+        const bot = new Telegraf(process.env.HOOK2)
+
+        bot.command('mama', async ctx=> {
+            try {
+                await ctx.reply('Mama yako ni selina')
+            } catch (err) {
+                console.log(err.message)
+            }
+        })
+
+        bot.on('message', async ctx => {
+            try {
+                await ctx.reply('Hello, welcome')
+            } catch (error) {
+                console.log(error.message)
+            }
+        })
+
+
+        if(process.env.ENVIRONMENT == 'production') {
+            //
+        } else {
+            bot.launch().catch(e=> console.log(e.message))
+        }
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+module.exports = {
+    bot2Fn
+}
--- routes/posts/post.js
+++ routes/posts/post.js
@@ -1,9 +1,18 @@
 const router = require('express').Router()
 const { bot1Fn } = require('./bots/bot1/bot1')
+const { bot2Fn } = require('./bots/bot2/bot2')
 
 router.post('/webhook/bot1', async (req, res)=> {
     try {
         await bot1Fn(router)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+router.post('/webhook/bot2', async (req, res)=> {
+    try {
+        await bot2Fn()
     } catch (error) {
         console.log(error.message)
     }
